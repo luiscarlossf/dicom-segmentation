5abd70047ce6cb550f1cf73f73dc3658e8b24310
luiscarlossf <luiscarlos.sf@outlook.com>
2019-05-24 14:50:29 -0300

    Encontra a marcação e exibe no slice

--- adaptative-matching.py
+++ adaptative-matching.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import pydicom
 from random import choice
+from skimage.exposure import rescale_intensity
+from dicompylercore import dicomparser
 #ADAPTIVE TEMPLATE MATCHING
 
 def show(pixel_array):
@@ -33,24 +35,48 @@ def load_files(path):
 ###########Suponha que o volume tenha sido selecionado aleatoriamente
 volume = load_files("C:/Users/luisc/Documents/dicom-database/LCTSC/LCTSC-Train-S1-001")
 
-test = pydicom.dcmread("C:/Users/luisc/Documents/dicom-database/LCTSC/LCTSC-Train-S1-001/11-16-2003-RTRCCTTHORAX8FLow Adult-39664/1-.simplified-62948/000000.dcm")
-print(test)
+marking = dicomparser.DicomParser("C:/Users/luisc/Documents/dicom-database/LCTSC/LCTSC-Train-S1-001/11-16-2003-RTRCCTTHORAX8FLow Adult-39664/1-.simplified-62948/000000.dcm")
+index_slice = 75
+markings = pydicom.dcmread("C:/Users/luisc/Documents/dicom-database/LCTSC/LCTSC-Train-S1-001/11-16-2003-RTRCCTTHORAX8FLow Adult-39664/1-.simplified-62948/000000.dcm")
+contoursequence = markings.ROIContourSequence[0].ContourSequence
+print(markings.ROIContourSequence[0].ReferencedROINumber)
+print(contoursequence[index_slice].ContourImageSequence)
+contour = contoursequence[index_slice].ContourData
+contour = np.array(contour).reshape(contoursequence[index_slice].NumberOfContourPoints, 3)
 #Gerando o template padrão
-#- Seleciona aleatoriamente um volume dentre todos os outros do banco de dados. 1.2.840.10008.5.1.4.1.1.481.3
-slice = choice(volume)
-#- Encontrar a marcação do especialista, encontrar o centro da massa dessa marcação;
-print(slice)
-show(slice.pixel_array)
+#- Seleciona aleatoriamente um volume dentre todos os outros do banco de dados.
+slice = None
+
+for index, i in enumerate(volume):
+    if i.SOPInstanceUID == contoursequence[index_slice].ContourImageSequence[0].ReferencedSOPInstanceUID:
+        print("Index: ", index)
+        slice = i
+print(slice.SOPInstanceUID)
+print(slice.ImagePositionPatient[0])
+rows = ((contour[:, 1] - slice.ImagePositionPatient[1])/slice.PixelSpacing[1]).astype('int')
+columns = ((contour[:, 0] - slice.ImagePositionPatient[0])/slice.PixelSpacing[0]).astype('int')
+pixels = np.copy(slice.pixel_array)
+pixels[rows, columns] = 65535
+
+show(pixels)
+
+#- Encontrar a marcação do especialista, encontrar o centro da massa dessa marcação
+
 #- Recortar duas vezes o tamanho da região correspondente de todos os lados.
 
 #Gerando o template inicial
 #- O algoritmo Template Matching é executado em cada slice do volume, onde o template é o
 #template padrão definido anteriormente;
+
 #- Calcular a similaridade em cada slice;
+
 #- O template inicial selecionado é aquele que tem maior similaridade com o template padrão,
 #o número do slice do melhor template inicial é salvo.
+
 #Adaptando o template para cada slice
 #- Executa o Template Matching no primeiro slice onde a combinação ocorre, como resultado temos:
 #o Novo Template e uma imagem.
+
 #- O Novo Template é executado no próximo slice e, novamente, o resultado será um Novo Template e uma image.
+
 #- No final, teremos um conjunto de imagens onde á apenas a região da medula segmentada.
