aa5eed480aad215b0723165c1ea88d3165880d12
Luis <luiscarlos.sf@outlook.com>
2020-03-30 21:52:34 -0300

    Deleta arquivos desnecessários.

---
+++ tests.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from superpixel import get_coordinates
+
+image = cv2.imread("./outputs/saida1.png", 0)
+
+#Calcula os superpixels da imagem
+image_ = np.copy(image)
+superpixels = cv2.ximgproc.createSuperpixelLSC(image_, 40)
+superpixels.iterate(20)
+s_number = superpixels.getNumberOfSuperpixels()
+print("O número de superpixels encontrados: {}.".format(s_number))
+masks = superpixels.getLabelContourMask()
+labels = superpixels.getLabels()
+
+#Obtem as coordenadas e as adjacências de cada superpixels.
+coords, adjcs = get_coordinates(labeled_image=labels,masks=masks, length=s_number)
+
+zeros = np.zeros(image_.shape, dtype=np.uint8)
+zeros[masks==255] = 255
+for c in coords:
+    coords[c][0] = np.array(coords[c][0])
+    coords[c][1] = np.array(coords[c][1])
+    mean_r = int(coords[c][0].mean())
+    mean_c = int(coords[c][1].mean())
+    zeros[(mean_r, mean_c)] = 255
+
+zeros[coords[100]] = 255
+cv2.imwrite("./outputs/test.png", zeros)
