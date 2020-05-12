41ed342a1e521515427d78d321af72fd8419dd9a
Luis <luiscarlos.sf2017@gmail.com>
2020-05-12 11:43:09 -0300

    Define função de similaridade

--- graphcut.py
+++ graphcut.py
@@ -2,18 +2,37 @@ import numpy as np
 import networkx as nx
 import math
 import matplotlib.pyplot as plt
+def distance(x1, x2):
+    if type(x1) == type((0,0)):
+        return math.sqrt(((x1[0] - x2[0])**2) + ((x1[1]-x2[1])**2))
+    return math.sqrt((x1 - x2)**2)
+
+def similarity_distance(i1, i2, d1, d2, di, dd, r):
+    intensity = distance(i1, i2)
+    distances = distance(d1, d2)
+    if distances < r:
+        return math.exp(-(intensity**2/di)) * math.exp(-(distances**2/dd))
+    else:
+        return 0
 
 def cut(graph):
-    laplacian = nx.laplacian_matrix(graph).toarray()
-    #print(np.linalg.eigh(laplacian))
-    eignvector = np.array(nx.fiedler_vector(graph))
+    if (len(graph.nodes) < 2):
+        return list(graph.nodes)
+    laplacian = nx.normalized_laplacian_matrix(graph).toarray()
+    eignvector = np.linalg.eigh(laplacian)[1][:,1]
+    eignvalues = nx.normalized_laplacian_spectrum(graph)
     #print("The second smallest eignvalue: {0}\nEignvector: {1}".format(result[0][1], eignvector))
+    print("Eignvalues {0}".format(eignvalues))
     print("Eignvector {0}".format(eignvector))
-    histogram = np.histogram(eignvector, bins=np.size(eignvector), density=True)
-    ratio =  (np.max(histogram[0])/np.min(histogram[0]) - 1) * 100
-    print("Histogram {0}\nRatio {1}".format(histogram, ratio))
-    threshold = np.mean(eignvector)
-    if ((eignvector < threshold).all() or (eignvector >= threshold).all()) or (len(graph.nodes) <= 2):
+    histogram = np.histogram(eignvalues, bins=np.size(eignvalues), density=True)
+    if len(eignvalues) > 2:
+        ratio = eignvalues[2] - eignvalues[1]
+    else:
+        ratio = eignvalues[1] - eignvalues[1]
+    print("Ratio {0:.2f}".format(ratio))
+    threshold = 0 #np.mean(eignvector)
+    res = input("Divide?")
+    if res == 'n': #(ratio < 0.06) :#((eignvector < threshold).all() or (eignvector >= threshold).all()) or (len(graph.nodes) <= 2): #len(graph.nodes) <= 2
         return list(graph.nodes)
     else:
         sub1 = graph.copy()
@@ -27,43 +46,24 @@ def cut(graph):
 if __name__ == "__main__":
     g = nx.Graph()
     g.add_nodes_from([1,2,3,4,5,6])
-    g.nodes[1]['info'] = {'dist': (0,0), 'color': 40}
-    g.nodes[2]['info'] = {'dist': (1,0), 'color': 30}
-    g.nodes[3]['info'] = {'dist': (0,1), 'color': 1}
-    g.nodes[4]['info'] = {'dist': (1,1), 'color': 2}
+    g.nodes[1]['info'] = {'dist': (0,0), 'color': 1}
+    g.nodes[2]['info'] = {'dist': (1,0), 'color': 2}
+    g.nodes[3]['info'] = {'dist': (0,1), 'color': 20}
+    g.nodes[4]['info'] = {'dist': (1,1), 'color': 20}
     g.nodes[5]['info'] = {'dist': (0,2), 'color': 50}
     g.nodes[6]['info'] = {'dist': (1,2), 'color': 60}
-    g.add_edges_from([(1,2), (1,3), (2,1), (2, 4), (3, 1), (3, 4), (3, 5), (4, 2), (4, 3), (4, 6), (5, 3), (5, 6), (6, 4), (6, 5)], weight=0)
+    g.add_edges_from([(1,2), (1,3), (2, 4), (3, 4), (3, 5), (4, 6), (5, 6)], weight=0)
     r = 2
-    colors = list()
-    distances = list()
-
-    for i in g.nodes:
-        colors.append(g.nodes[i]['info']['color'])
-        for j in g[i]:
-            d1 = g.nodes[i]['info']['dist']
-            d2 = g.nodes[j]['info']['dist']
-            distances.append(math.sqrt((((d1[0]-d2[0])**2)+((d1[1]-d2[1])**2))))
-    aux = [((color - np.mean(colors))**2) for color in colors]
-    deviation_colors = math.sqrt(sum(aux)/len(aux))
-    aux = [((dist - np.mean(distances))**2) for dist in distances]
-    deviation_distances = math.sqrt(sum(aux)/len(aux))
-    for i in g.nodes:
+    
+    for (i, j) in g.edges:
         color1 = g.nodes[i]['info']['color']
-        soma_ = 0
-        for j in g[i]:
-            color2 = g.nodes[j]['info']['color']
-            mean = (color1 + color2)/2
-            soma = ((color1 - mean)**2) + ((color2 - mean)**2)
-            p1 = math.sqrt((color1 - color2) ** 2)
-            p2 = p1 / ((deviation_colors**2))
-            d1 = g.nodes[i]['info']['dist']
-            d2 = g.nodes[j]['info']['dist']
-            p3 = (math.sqrt((((d1[0]-d2[0])**2)+((d1[1]-d2[1])**2)))) #/ ((deviation_distances ** 2) + 1e-11)
-            if p3 < r:
-                g[i][j]['weight'] =  math.exp(-(p2)) #* math.exp(-p3) #math.exp(-(abs(color1 - color2) * abs(color1 - color2))/(2*((math.sqrt(soma/2))**2)))
-            else:
-                g[i][j]['weight'] = 0
+        color2 = g.nodes[j]['info']['color']
+        d1 = g.nodes[i]['info']['dist']
+        d2 = g.nodes[j]['info']['dist']
+        g[i][j]['weight'] =  similarity_distance(color1, color2, d1, d2, 51, 102.4, 2)
+    laplacian = nx.laplacian_matrix(g).toarray()
+    eign = np.linalg.eigh(laplacian)
+    print(eign)
     print(cut(g))
     #nx.draw(g, with_labels=True, font_weight='bold')
     #plt.show()
